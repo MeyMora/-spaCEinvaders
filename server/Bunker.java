ff89b4540794feddb1a34c68ff0ea1eafb87181e
MeyMora <112515882+MeyMora@users.noreply.github.com>
2026-05-18 20:11:18 -0600

    feat(java): Se agrega la clase Bunker.java

---
+++ server/Bunker.java
@@ -0,0 +1,29 @@
+public class Bunker {
+    private int id; // Variable id para identificar cual bunker es, si el 1, 2,3...
+    private int health; //Variable privada que es la vida actual del bunker
+
+    public Bunker(int id){  //Constructor de la clase de Bunker
+        this.id = id; // Se asigna el identificador del bunker.
+        this.health = 100; // la vida del bunker siempre inicia en 100
+    }
+    //Metodo que devuelve el id del bunker
+    public int getId() {
+        return id;
+    }
+    // Metodo que devuelve la vida actual del bunker
+    public int getHealth() {
+        return health;
+    }
+    // Metodo que disminuye la vida del bunker según el daño recibido.
+    public void damage(int damage) {
+        health -= damage; // Se resta el daño a la vida actual.
+        if (health <= 0) { // Si la vida llega a 0 o menos, se mantiene en 0.
+            health = 0;
+        }
+    }
+    //Metodo que devuelve un texto con la informacion del bunker, retorna una cadena de texto
+    public String toMessage() {
+        return "BUNKER " + id + " " + health;
+    }
+
+}
